3cd1532c8a19fdbd47047bf03939085cf5ea2a5a
haojiahong <haojiahong@treefinance.com.cn>
2017-08-07 14:24:09 +0800

    合计所有商户后的监控和预警

--- biz/src/main/java/com/treefinance/saas/monitor/biz/service/impl/AllAlarmServiceImpl.java
+++ biz/src/main/java/com/treefinance/saas/monitor/biz/service/impl/AllAlarmServiceImpl.java
@@ -10,6 +10,7 @@ import com.treefinance.saas.monitor.dao.mapper.SaasStatAccessMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -18,6 +19,7 @@ import java.util.List;
 /**
  * Created by haojiahong on 2017/8/4.
  */
+@Service
 public class AllAlarmServiceImpl implements AllAlarmService {
 
     private static final Logger logger = LoggerFactory.getLogger(AlarmServiceImpl.class);
