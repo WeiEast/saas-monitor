ce975a7ddeefb52505e1cf764803184404a20a02
haojiahong <haojiahong@treefinance.com.cn>
2017-09-13 14:15:54 +0800

    在预警中，支持exclude部分AppID,输出文档时也要排除这部分商户

--- biz/src/main/java/com/treefinance/saas/monitor/biz/service/impl/AllAlarmServiceImpl.java
+++ biz/src/main/java/com/treefinance/saas/monitor/biz/service/impl/AllAlarmServiceImpl.java
@@ -1,20 +1,30 @@
 package com.treefinance.saas.monitor.biz.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.treefinance.saas.monitor.biz.config.DiamondConfig;
 import com.treefinance.saas.monitor.biz.mq.producer.AlarmMessageProducer;
 import com.treefinance.saas.monitor.biz.service.AllAlarmService;
 import com.treefinance.saas.monitor.common.enumeration.EStatType;
+import com.treefinance.saas.monitor.dao.entity.MerchantStatAccess;
+import com.treefinance.saas.monitor.dao.entity.MerchantStatAccessCriteria;
 import com.treefinance.saas.monitor.dao.entity.SaasStatAccess;
 import com.treefinance.saas.monitor.dao.entity.SaasStatAccessCriteria;
+import com.treefinance.saas.monitor.dao.mapper.MerchantStatAccessMapper;
 import com.treefinance.saas.monitor.dao.mapper.SaasStatAccessMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by haojiahong on 2017/8/4.
@@ -27,6 +37,10 @@ public class AllAlarmServiceImpl implements AllAlarmService {
     private AlarmMessageProducer alarmMessageProducer;
     @Autowired
     private SaasStatAccessMapper saasStatAccessMapper;
+    @Autowired
+    private MerchantStatAccessMapper merchantStatAccessMapper;
+    @Autowired
+    private DiamondConfig diamondConfig;
 
 
     @Override
@@ -41,6 +55,47 @@ public class AllAlarmServiceImpl implements AllAlarmService {
                 .andDataTimeIn(alarmTimes)
                 .andDataTypeEqualTo(type.getType());
         List<SaasStatAccess> list = saasStatAccessMapper.selectByExample(criteria);
+
+        //获取需要排除的商户的统计信息
+        List<String> excludeAppIds = Lists.newArrayList();
+        if (StringUtils.isNotBlank(diamondConfig.getMonitorAlarmExcludeAppIdsAll())) {
+            excludeAppIds = Splitter.on(",").trimResults().splitToList(diamondConfig.getMonitorAlarmExcludeAppIdsAll());
+            logger.info("TaskMonitorAlarm: excludeAppIds={}", JSON.toJSONString(excludeAppIds));
+        }
+        if (!CollectionUtils.isEmpty(excludeAppIds)) {
+            MerchantStatAccessCriteria merchantCriteria = new MerchantStatAccessCriteria();
+            merchantCriteria.setOrderByClause("dataTime desc");
+            merchantCriteria.createCriteria().andAppIdIn(excludeAppIds)
+                    .andDataTimeIn(alarmTimes)
+                    .andDataTypeEqualTo(type.getType());
+            List<MerchantStatAccess> merchantStatAccessList = merchantStatAccessMapper.selectByExample(merchantCriteria);
+            logger.info("TaskMonitorAlarm: excludeMerchantStatAccessList={}", JSON.toJSONString(merchantStatAccessList));
+            Map<Date, List<MerchantStatAccess>> excludeMerchantMap = merchantStatAccessList.stream().collect(Collectors.groupingBy(MerchantStatAccess::getDataTime));
+            List<SaasStatAccess> excludedList = Lists.newArrayList();
+            for (SaasStatAccess data : list) {
+                SaasStatAccess saasStatAccess = new SaasStatAccess();
+                BeanUtils.copyProperties(data, saasStatAccess);
+                List<MerchantStatAccess> excludeMerchantList = excludeMerchantMap.get(data.getDataTime());
+                int excludeTotalCount = 0, excludeSuccessCount = 0, excludeFailCount = 0, excludeCancelCount = 0;
+                for (MerchantStatAccess merchantStatAccess : excludeMerchantList) {
+                    excludeTotalCount = excludeTotalCount + merchantStatAccess.getTotalCount();
+                    excludeSuccessCount = excludeSuccessCount + merchantStatAccess.getSuccessCount();
+                    excludeFailCount = excludeFailCount + merchantStatAccess.getFailCount();
+                    excludeCancelCount = excludeCancelCount + merchantStatAccess.getCancelCount();
+                }
+                int totalCount = saasStatAccess.getTotalCount() == null ? 0 : saasStatAccess.getTotalCount();
+                int successCount = saasStatAccess.getSuccessCount() == null ? 0 : saasStatAccess.getSuccessCount();
+                int failCount = saasStatAccess.getFailCount() == null ? 0 : saasStatAccess.getFailCount();
+                int cancelCount = saasStatAccess.getCancelCount() == null ? 0 : saasStatAccess.getCancelCount();
+
+                saasStatAccess.setTotalCount(totalCount - excludeTotalCount);
+                saasStatAccess.setSuccessCount(successCount - excludeSuccessCount);
+                saasStatAccess.setFailCount(failCount - excludeFailCount);
+                saasStatAccess.setCancelCount(cancelCount - excludeCancelCount);
+                excludedList.add(saasStatAccess);
+            }
+            list = excludedList;
+        }
         alarmMessageProducer.sendMail4All(list, type);
         alarmMessageProducer.sendWebChart4All(list, type);
     }
