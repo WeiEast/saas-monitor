2fcfd2b06f5ad8af45f28abe9cb5d877545b1d8e
haojiahong <haojiahong@treefinance.com.cn>
2018-01-15 11:24:49 +0800

    电商监控统计

---
+++ biz/src/main/java/com/treefinance/saas/monitor/biz/facade/EcommerceStatAccessFacadeImpl.java
@@ -0,0 +1,13 @@
+package com.treefinance.saas.monitor.biz.facade;
+
+import com.treefinance.saas.monitor.facade.service.stat.EcommerceStatAccessFacade;
+import org.springframework.stereotype.Service;
+
+/**
+ * 电商监控统计
+ * Created by haojiahong on 2018/1/15.
+ */
+@Service("ecommerceStatAccessFacade")
+public class EcommerceStatAccessFacadeImpl implements EcommerceStatAccessFacade {
+
+}
---
+++ facade/src/main/java/com/treefinance/saas/monitor/facade/service/stat/EcommerceStatAccessFacade.java
@@ -0,0 +1,9 @@
+package com.treefinance.saas.monitor.facade.service.stat;
+
+/**
+ * 电商监控统计
+ * Created by haojiahong on 2018/1/15.
+ */
+public interface EcommerceStatAccessFacade {
+
+}
