64cf6c23cd3560790d6a59dd82f1ae4b8016db09
haojiahong <haojiahong@treefinance.com.cn>
2018-03-02 10:39:18 +0800

    任务成功率预警,不同时间不同阈值

--- common/src/main/java/com/treefinance/saas/monitor/common/domain/dto/TaskSuccessRateAlarmConfigDTO.java
+++ common/src/main/java/com/treefinance/saas/monitor/common/domain/dto/TaskSuccessRateAlarmConfigDTO.java
@@ -1,6 +1,10 @@
 package com.treefinance.saas.monitor.common.domain.dto;
 
+import com.alibaba.rocketmq.shade.com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by haojiahong on 2017/11/24.
@@ -56,6 +60,37 @@ public class TaskSuccessRateAlarmConfigDTO implements Serializable {
      * 短信预警开关
      */
     private String smsAlarmSwitch;
+
+    public static void main(String[] args) {
+        List<TaskSuccessRateAlarmConfigDTO> list = Lists.newArrayList();
+        TaskSuccessRateAlarmConfigDTO dto1 = new TaskSuccessRateAlarmConfigDTO();
+        dto1.setType("OPERATOR");
+        dto1.setSuccesThreshold(20);
+        dto1.setIntervalMins(3);
+        dto1.setTimes(3);
+        dto1.setTaskTimeoutSecs(600);
+        dto1.setAlarmStartTime("0:00");
+        dto1.setAlarmEndTime("7:00");
+        dto1.setMailAlarmSwitch("on");
+        dto1.setWeChatAlarmSwitch("on");
+        dto1.setSmsAlarmSwitch("on");
+
+        TaskSuccessRateAlarmConfigDTO dto2 = new TaskSuccessRateAlarmConfigDTO();
+        dto2.setType("OPERATOR");
+        dto2.setSuccesThreshold(40);
+        dto2.setIntervalMins(3);
+        dto2.setTimes(3);
+        dto2.setTaskTimeoutSecs(600);
+        dto2.setAlarmStartTime("7:00");
+        dto2.setAlarmEndTime("24:00");
+        dto2.setMailAlarmSwitch("on");
+        dto2.setWeChatAlarmSwitch("on");
+        dto2.setSmsAlarmSwitch("on");
+
+        list.add(dto1);
+        list.add(dto2);
+        System.out.println(JSON.toJSONString(list));
+    }
 
 
     public String getType() {
