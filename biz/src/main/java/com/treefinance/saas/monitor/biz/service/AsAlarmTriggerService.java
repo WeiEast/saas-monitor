fdfbdf950f788a6acb5db9085ab262d6f783946c
guoguoyun <guoguoyun@treefinance.com.cn>
2018-07-18 20:27:00 +0800

    增加GroupStatFacade 统计分组数据dubbo服务接口（3）

---
+++ biz/src/main/java/com/treefinance/saas/monitor/biz/service/AsAlarmTriggerService.java
@@ -0,0 +1,14 @@
+package com.treefinance.saas.monitor.biz.service;
+
+import com.treefinance.saas.monitor.dao.entity.AsAlarmTrigger;
+
+/**
+ * @author:guoguoyun
+ * @date:Created in 2018/7/18下午8:15
+ */
+public interface AsAlarmTriggerService {
+
+
+     AsAlarmTrigger getAsAlarmTriggerByID(long id);
+
+}
