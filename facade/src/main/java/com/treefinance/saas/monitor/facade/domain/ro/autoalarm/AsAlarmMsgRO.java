0589ecd6b3e4fd833a7321695711ca567b71bb20
haojiahong <haojiahong@treefinance.com.cn>
2018-07-19 14:19:32 +0800

    预警基本配置RO

--- facade/src/main/java/com/treefinance/saas/monitor/facade/domain/ro/autoalarm/AsAlarmMsgRO.java
+++ facade/src/main/java/com/treefinance/saas/monitor/facade/domain/ro/autoalarm/AsAlarmMsgRO.java
@@ -1,11 +1,15 @@
 package com.treefinance.saas.monitor.facade.domain.ro.autoalarm;
 
 import com.treefinance.saas.monitor.facade.domain.base.BaseRO;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author haojiahong
  * @date 2018/7/19
  */
+@Getter
+@Setter
 public class AsAlarmMsgRO extends BaseRO {
     private Long id;
     /**
